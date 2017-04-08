2fdc5ebd0d22fd12fd387b23dfdbe77f6139be04
Jonathan Campbell <jonathan@castus.tv>
2017-04-07 22:52:21 -0700

    try to stay with the parent dir of a demo, not it's subdirs

--- yet-to-test.pl
+++ yet-to-test.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
+my $skip = '';
 open(X,"find -type d |") || die;
 while (my $path = <X>) {
     chomp $path;
     next unless -d $path;
     next if $path =~ m/\'/;
+    next if ($skip ne '' && (substr($path,0,length($skip)) eq $skip));
 
     # skip if it already has __PASS__ or __FAIL__
     next if ( -f "$path/__PASS__" || -f "$path/__FAIL__" );
@@ -13,6 +15,8 @@ while (my $path = <X>) {
     next if $x eq "";
 
     print "$path\n";
+
+    $skip = $path;
 }
 close(X);
 
