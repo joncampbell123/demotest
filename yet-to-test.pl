1d7660488953e651dac71215bba95001a6ee441c
Jonathan Campbell <jonathan@castus.tv>
2017-04-24 08:37:27 -0700

    more

--- yet-to-test.pl
+++ yet-to-test.pl
@@ -10,9 +10,6 @@ while (my $path = <X>) {
     # skip if it already has __PASS__ or __FAIL__
     next if ( -f "$path/__PASS__" || -f "$path/__FAIL__" );
 
-    # TEMP: pick on 1994
-    next unless $path =~ m/1994/;
-
     # skip unless it has an EXE or COM file
     $x=`cd '$path' && ls *.EXE *.COM 2>/dev/null | head -n 1`; chomp $x;
     next if $x eq "";
