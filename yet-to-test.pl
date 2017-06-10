77cf15acc2e2f3aa0fff98c077bfc3de7cb115e6
Jonathan Campbell <jonathan@castus.tv>
2017-06-10 00:03:38 -0700

    more

--- yet-to-test.pl
+++ yet-to-test.pl
@@ -7,6 +7,9 @@ while (my $path = <X>) {
     next if $path =~ m/\'/;
     next if ($skip ne '' && (substr($path,0,length($skip)) eq $skip));
 
+    # TEMP: we only want 1994 and earlier demos
+    next unless $path =~ m/\/199[0-4]\//;
+
     # skip if it already has __PASS__ or __FAIL__
     next if ( -f "$path/__PASS__" || -f "$path/__FAIL__" );
 
