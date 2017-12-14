ce91b46a6ee44164745f5c070e644194eb483a62
Jonathan Campbell <jonathan@castus.tv>
2017-12-13 16:46:21 -0800

    more

--- pick-one-and-enter.sh
+++ pick-one-and-enter.sh
@@ -52,6 +52,7 @@ cp -vn dosbox-pentium.conf dosbox-template.conf || exit 1
 cp -vn dosbox-template.conf "$x/dosbox.conf" || exit 1
 cp -vn mapper-0.801.map "$x/mapper-0.801.map" || exit 1
 cp -vn mapper-0.801.map "$x/mapper-0.81.map" || exit 1
+cp -vn mapper-0.801.map "$x/mapper-0.82.map" || exit 1
 
 (cd "$x" && echo "I entered the directory by running your shell again. Type 'exit' to exit out of it." && bash) || exit 1
 
