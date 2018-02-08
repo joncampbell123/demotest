a863daf91d1ea1a50005ac4787085e28386d9b90
Jonathan Campbell <jonathan@castus.tv>
2018-02-08 14:20:38 -0800

    more

--- pick-one.sh
+++ pick-one.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 x=$RANDOM
 x=$(($x%1000))
-./yet-to-test.pl | head -n $x | tail -n 1
+
+suffix=
+if [[ "$1" == "svn" ]]; then suffix="-svn"; fi
+
+./yet-to-test$suffix.pl | head -n $x | tail -n 1
 
