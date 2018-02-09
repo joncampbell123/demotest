d3a8fcc7802faa5b0933c91fa41a03af3ac51fb4
Jonathan Campbell <jonathan@castus.tv>
2018-02-09 02:47:55 -0800

    more

--- pick-one.sh
+++ pick-one.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 x=$RANDOM
-x=$(($x%1000))
+x=$((($x%1000)+1))
+
+# NTS: For the time being, I want to test oldest first
+x=1
 
 suffix=
 if [[ "$1" == "svn" ]]; then suffix="-svn"; fi
 
-./yet-to-test$suffix.pl | head -n $x | tail -n 1
+./yet-to-test$suffix.pl | sort | head -n $x | tail -n 1
 
