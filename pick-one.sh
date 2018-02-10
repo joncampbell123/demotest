e9b8b9c91f9e5069c7c13c017c3dd271aad6e24c
Jonathan Campbell <jonathan@castus.tv>
2018-02-09 20:43:15 -0800

    more

--- pick-one.sh
+++ pick-one.sh
@@ -8,9 +8,5 @@ x=1
 suffix=
 if [[ "$1" == "svn" ]]; then suffix="-svn"; fi
 
-if [[ !( -f pick-one.cache ) ]]; then
-    (./yet-to-test$suffix.pl | sort >pick-one.cache) || exit 1
-fi
-
-cat pick-one.cache | head -n $x | tail -n 1
+./yet-to-test$suffix.pl | head -n $x | tail -n 1
 
