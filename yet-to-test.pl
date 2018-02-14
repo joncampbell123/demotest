71aca56c5045bda4328ca4b99997801047800bff
Jonathan Campbell <jonathan@castus.tv>
2018-02-14 00:29:40 -0800

    more

--- yet-to-test.pl
+++ yet-to-test.pl
@@ -26,7 +26,7 @@ while (my $path = <X>) {
     next if ($skip ne '' && (substr($path,0,length($skip)) eq $skip));
 
     # TEMP: we only want 1995 and earlier demos
-    next unless ($path =~ m/\/199[0-5]\// || $path =~ m/\/198[0-9]\//);
+#    next unless ($path =~ m/\/199[0-5]\// || $path =~ m/\/198[0-9]\//);
 
     # skip Amiga demos, we can't run them
     next if $path =~ m/\/amiga\/demo\//;
