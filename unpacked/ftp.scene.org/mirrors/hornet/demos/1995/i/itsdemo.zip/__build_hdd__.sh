6a001814836ff9f69a951117d974662df74c146f
Jonathan Campbell <jonathan@castus.tv>
2018-04-04 17:54:43 -0700

    more

---
+++ unpacked/ftp.scene.org/mirrors/hornet/demos/1995/i/itsdemo.zip/__build_hdd__.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# FIXME
+hddimgdir=/mnt/main/emu/demotest
+hddimg=$hddimgdir/msdos622hdd.16mb.vdi
+prtofs=$((21*512))
+
+rm -f __hdd__ || exit 1
+qemu-img convert -O raw $hddimg __hdd__ || exit 1
+
+# DEMO
+mmd -i __hdd__@@$prtofs demo || exit 1
+mcopy -i __hdd__@@$prtofs -b *.* ::demo || exit 1
+
+# mouse
+mcopy -i __hdd__@@$prtofs -b $hddimgdir/ctmouse.exe ::ctmouse.exe || exit 1
+
+# configure
+mdel -i __hdd__@@$prtofs ::autoexec.bat ::config.sys || exit 1
+# config.sys
+mcopy -i __hdd__@@$prtofs -t - ::config.sys <<_EOF
+DEVICE=C:\DOS\HIMEM.SYS
+DOS=HIGH,UMB
+_EOF
+# autoexec.bat
+mcopy -i __hdd__@@$prtofs -t - ::autoexec.bat <<_EOF
+@echo off
+SET PATH=C:\DOS
+\CTMOUSE.EXE
+CD \DEMO
+ITSDEMO.EXE
+ECHO ---- END OF DEMO ----
+_EOF
+
