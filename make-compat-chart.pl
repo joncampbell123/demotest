9a32d8cb517b92c048b27bf7aed2dcd4a2ec201a
Jonathan Campbell <jonathan@castus.tv>
2018-07-15 10:22:50 -0700

    more

--- make-compat-chart.pl
+++ make-compat-chart.pl
@@ -242,7 +242,7 @@ while ($line = <S>) {
 
         # skip unless it has an EXE or COM file
         $path_esc=escape_shell($line);
-        $x=`cd $path_esc && ls *.exe *.EXE *.com *.COM 2>/dev/null | head -n 1`; chomp $x;
+        $x=`cd $path_esc && ls -- *.exe *.EXE *.com *.COM 2>/dev/null | head -n 1`; chomp $x;
         next if $x eq "";
     }
 
